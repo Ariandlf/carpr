f6d918a79d21ceb6cce1c56dc5f396a6fae28e80
Ariandlf <133434092+Ariandlf@users.noreply.github.com>
2024-04-16 20:43:22 +0330

    car-site

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/42710811-3a8d-498f-bbd8-510e6f98d21f/x8xP2XJD7U.json'
+})
